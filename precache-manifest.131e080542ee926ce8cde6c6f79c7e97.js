9a59b9c19692f99d05e54fbbb1ee8806e07a296d
Anton Samoilenko <antonplane@gmail.com>
2021-07-15 14:43:00 +0300

    test

---
+++ precache-manifest.131e080542ee926ce8cde6c6f79c7e97.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a302dbd0c64467460e58",
+    "url": "css/app.09c418ac.css"
+  },
+  {
+    "revision": "85bd287eee7b7fcd3087",
+    "url": "css/chunk-vendors.902ebb66.css"
+  },
+  {
+    "revision": "66819e80b6a8c4e71b46",
+    "url": "css/layout-components-modal-vue.2cd37f8c.css"
+  },
+  {
+    "revision": "db9ff0c7bde3939cca44",
+    "url": "css/layout-components-room-vue.a2ef6f22.css"
+  },
+  {
+    "revision": "73dab7914c63ec846ab8",
+    "url": "css/layout-layouts-auth-vue.3805db56.css"
+  },
+  {
+    "revision": "4db86eb03eecd60742da",
+    "url": "css/layout-layouts-default-vue.7386cdea.css"
+  },
+  {
+    "revision": "608c1ebc4daf1ff331e0",
+    "url": "css/layout-views-login-vue.04fa0319.css"
+  },
+  {
+    "revision": "ad3a2a2e61fec2b0aad8",
+    "url": "css/layout-views-member-vue.fc65f258.css"
+  },
+  {
+    "revision": "7a26c481a54c3795a8b7",
+    "url": "css/layout-views-members-vue.01081d78.css"
+  },
+  {
+    "revision": "77a99ee94df6706c7b1a",
+    "url": "css/layout-views-rooms-vue.f0f2c677.css"
+  },
+  {
+    "revision": "332a6d23e501a5965d80",
+    "url": "css/layout-views-statistics-vue.fdc00816.css"
+  },
+  {
+    "revision": "5b21fed14fa5b47f6460",
+    "url": "css/layout-views-utilityBilling-vue.4d506dcb.css"
+  },
+  {
+    "revision": "b6fd3078b336ba2978467caf144ec934",
+    "url": "data/control-bar.json"
+  },
+  {
+    "revision": "68aadbb7c81d10557302f66375e9a0bf",
+    "url": "data/devices.json"
+  },
+  {
+    "revision": "1b4ed50bcab0521fb32457bb7b10ec80",
+    "url": "data/invoice-history.json"
+  },
+  {
+    "revision": "5d96c85a9ea2d42a4fc09a2a7dcb5008",
+    "url": "data/members.json"
+  },
+  {
+    "revision": "638353aa0e3a620c0f50b73f50f1e1e0",
+    "url": "data/rooms.json"
+  },
+  {
+    "revision": "2229158ec087bc874a0ecdc36b9fdcd1",
+    "url": "favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "fdc2535870d21aac82a6e81fdbe59bc6",
+    "url": "favicon/android-chrome-256x256.png"
+  },
+  {
+    "revision": "2638b1826ec5161c173319b5a80b66ae",
+    "url": "favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "02fbd1fa5357cee2cef9acadfb33794f",
+    "url": "favicon/browserconfig.xml"
+  },
+  {
+    "revision": "df387a66fda4f88b8fff89f1581d46c2",
+    "url": "favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "2c19b2945ddc9eeb0f47e81af52bb46a",
+    "url": "favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "d2150d10a7fdd944986f08ca9cd86bf0",
+    "url": "favicon/mstile-150x150.png"
+  },
+  {
+    "revision": "1e215222c658f85b36eef20be226373e",
+    "url": "favicon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "97ab456c3cdf906a2bfe1147331c274e",
+    "url": "favicon/site.webmanifest"
+  },
+  {
+    "revision": "45428052f4bf5c3155b821d1cd2d6a38",
+    "url": "fonts/Lackitalic.45428052.woff2"
+  },
+  {
+    "revision": "53742e6696d2822e626cdb99980ef030",
+    "url": "fonts/Lackitalic.53742e66.woff"
+  },
+  {
+    "revision": "70646338319b5f982119a7a6b08a2fcb",
+    "url": "fonts/Lackitalic.70646338.ttf"
+  },
+  {
+    "revision": "29d3e158c1b8b016efca37c1d927bea1",
+    "url": "fonts/Lackregular.29d3e158.ttf"
+  },
+  {
+    "revision": "98fe4b03d46b8c826f04b2896342f056",
+    "url": "fonts/Lackregular.98fe4b03.woff2"
+  },
+  {
+    "revision": "cafd477eccfab49e78329cae1b02e536",
+    "url": "fonts/Lackregular.cafd477e.woff"
+  },
+  {
+    "revision": "b1fbfa99399657c1447942e96fcdc1e2",
+    "url": "images/icon-192x192.png"
+  },
+  {
+    "revision": "764ae5494a635193a526f5cf3c1f5cf0",
+    "url": "images/icon-256x256.png"
+  },
+  {
+    "revision": "75ffa82d838857489823ead6b7202624",
+    "url": "images/icon-384x384.png"
+  },
+  {
+    "revision": "b016e75e12d2e9f48dfd242b86fb64e0",
+    "url": "images/icon-512x512.png"
+  },
+  {
+    "revision": "3a08332c469d23a919d585a4712ea6e9",
+    "url": "images/og_img.jpg"
+  },
+  {
+    "revision": "d1c1c9286631f069e07c2b22bddfba83",
+    "url": "img/sprite.d1c1c928.svg"
+  },
+  {
+    "revision": "12320eb0cb17c43c67bd8e32fb9fec64",
+    "url": "index.html"
+  },
+  {
+    "revision": "a302dbd0c64467460e58",
+    "url": "js/app.e40cfc75.js"
+  },
+  {
+    "revision": "ea95050ebf423ada01a8",
+    "url": "js/canvg.b16d9a45.js"
+  },
+  {
+    "revision": "85bd287eee7b7fcd3087",
+    "url": "js/chunk-vendors.f6606097.js"
+  },
+  {
+    "revision": "66819e80b6a8c4e71b46",
+    "url": "js/layout-components-modal-vue.7a416a90.js"
+  },
+  {
+    "revision": "db9ff0c7bde3939cca44",
+    "url": "js/layout-components-room-vue.d4fd773a.js"
+  },
+  {
+    "revision": "73dab7914c63ec846ab8",
+    "url": "js/layout-layouts-auth-vue.c47a9229.js"
+  },
+  {
+    "revision": "4db86eb03eecd60742da",
+    "url": "js/layout-layouts-default-vue.5e4332dc.js"
+  },
+  {
+    "revision": "95803d2455da23823347",
+    "url": "js/layout-layouts-default-vue~layout-views-login-vue~layout-views-member-vue~layout-views-members-vue~l~5c914e80.a211d5c9.js"
+  },
+  {
+    "revision": "608c1ebc4daf1ff331e0",
+    "url": "js/layout-views-login-vue.c6f558f2.js"
+  },
+  {
+    "revision": "ad3a2a2e61fec2b0aad8",
+    "url": "js/layout-views-member-vue.595e3728.js"
+  },
+  {
+    "revision": "7a26c481a54c3795a8b7",
+    "url": "js/layout-views-members-vue.b781e88e.js"
+  },
+  {
+    "revision": "77a99ee94df6706c7b1a",
+    "url": "js/layout-views-rooms-vue.8b219228.js"
+  },
+  {
+    "revision": "332a6d23e501a5965d80",
+    "url": "js/layout-views-statistics-vue.f30b782f.js"
+  },
+  {
+    "revision": "540066a81e336491843b",
+    "url": "js/layout-views-statistics-vue~layout-views-utilityBilling-vue.eb474214.js"
+  },
+  {
+    "revision": "5b21fed14fa5b47f6460",
+    "url": "js/layout-views-utilityBilling-vue.dbfbaeaf.js"
+  },
+  {
+    "revision": "8474591c920c7d5cb2d6",
+    "url": "js/pdfmake.36c0bd2a.js"
+  },
+  {
+    "revision": "39895dd811d885f0476f",
+    "url": "js/xlsx.6cddc77e.js"
+  },
+  {
+    "revision": "56f2cd7635deb6df964af770520c0cc5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "f1a87ccbfb41994784fc88c724d5a0b8",
+    "url": "manifest.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
